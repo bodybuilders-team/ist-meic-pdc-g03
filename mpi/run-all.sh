f38403f78bd691872640bebbcdb57415eff597c0
André Jesus <andre.jesus.pilar@gmail.com>
2024-03-18 14:01:00 +0000

    Refactor run-all.sh script to redirect output to a file

--- mpi/run-all.sh
+++ mpi/run-all.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Output file
+output_file="output.txt"
+
 # Compile the program
-make clean && make
+(make clean && make) 2>&1 | tee -a "$output_file"
 
 # Define configurations
 configurations=(
@@ -10,9 +13,6 @@ configurations=(
     "10 512 0.4 0"
     "3 1024 0.4 100"
 )
-
-# Output file
-output_file="output.txt"
 
 # Define parallel tasks
 # TODO: change to control number of threads in each processor using OMP_NUM_THREADS
@@ -24,8 +24,12 @@ hosts="lab1p1,lab1p2,lab1p3,lab1p4,lab1p5,lab1p6,lab1p7,lab1p8,lab1p9,lab1p10"
 # Loop through each configuration
 for config in "${configurations[@]}"; do
     for processes in "${parallel_tasks[@]}"; do
-        echo "Executing with $processes MPI processes and configuration: $config"
-        mpirun -host $hosts -np $processes ./life3d-mpi $config >> "$output_file" 2>&1
+        echo "Executing with $processes MPI processes and configuration: $config" 
+        echo "Executing with $processes MPI processes and configuration: $config" >> "$output_file"
+
+        mpirun -host $hosts -np $processes ./life3d-mpi $config 2>&1 | tee -a "$output_file"
+        
         echo "Execution completed."
+        echo "Execution completed." >> "$output_file"
     done
 done
